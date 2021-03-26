e2fc0a87d0640ab4faaffe45580fa01f4485764e
Aura Arana <Aura.Arana001@mymdc.net>
2021-03-26 23:15:46 +0000

    added login

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,33 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
+import Background from "../../img/login-background.png";
+import Logo from "../../img/logo.png";
 import "../../styles/home.scss";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="row">
+		<div className="d-flex flex-col position-relative" style={{ width: 700 }}>
+			<img src={Background} className="card-img rounded-0" alt="..." />
+			<div className="card-img-overlay ">
+				<img src={Logo} style={{ width: 200 }} className="" />
+			</div>
+		</div>
+		<div className="d-flex flex-col position-relative" style={{ width: 700 }}>
+			<form className="position-absolute top-50 start-50 translate-middle">
+				<div className="mb-3">
+					<input type="email" className="form-control" placeholder="Enter Email" />
+				</div>
+				<div className="mb-3">
+					<input type="password" className="form-control" placeholder="Enter Password" />
+				</div>
+				<div className="mb-3 form-check">
+					<input type="checkbox" className="form-check-input" id="exampleCheck1" />
+					<label className="form-check-label" form="exampleCheck1">
+						Check me out
+					</label>
+				</div>
+				<button className="btn btn-primary">Submit</button>
+			</form>
+		</div>
 	</div>
 );
