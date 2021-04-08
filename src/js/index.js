35f539f19984c8feb1bfc0dfbd75f45d2a3d432b
jafinol <jafinol@gmail.com>
2021-04-08 23:38:05 +0000

    authentication firebase

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,9 @@ import ReactDOM from "react-dom";
 
 import $ from "jquery";
 import Popper from "popper.js";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/analytics";
 
 //include bootstrap npm library into the bundle
 import "bootstrap/dist/js/bootstrap.min.js";
@@ -16,6 +19,19 @@ import "../styles/index.scss";
 
 //import your own components
 import Layout from "./layout";
+
+var firebaseConfig = {
+	apiKey: "AIzaSyAIYVsPhQI3wb6QXsRnhn8xB_RgauGfmIY",
+	authDomain: "mdc-auth-edfb1.firebaseapp.com",
+	projectId: "mdc-auth-edfb1",
+	storageBucket: "mdc-auth-edfb1.appspot.com",
+	messagingSenderId: "740672945803",
+	appId: "1:740672945803:web:5c2701e7c50b2f82ae9e8e",
+	measurementId: "G-R7DQRT041Q"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 //render your react application
 ReactDOM.render(<Layout />, document.querySelector("#app"));
