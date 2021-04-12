1e0be7cc10867fe1c5a1eac814b19f755877693a
Aura Arana <Aura.Arana001@mymdc.net>
2021-04-12 19:19:17 +0000

    edited report

--- src/js/views/caseReport.js
+++ src/js/views/caseReport.js
@@ -2,6 +2,7 @@ import React, { useState, useContext, useEffect } from "react";
 import { Link, useHistory, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 import PropTypes from "prop-types";
+import { Table } from "react-bootstrap";
 
 export const CaseReport = () => {
 	let addBio = "Case";
@@ -85,27 +86,219 @@ export const CaseReport = () => {
 	return (
 		<div className="container">
 			<div>
-				<h1 className="text-center mt-5">{params.case}</h1>
+				<h1 className="h3 text-gray-800 text-center mt-5">{params.case}</h1>
 
-				<div className="container-fluid">
+				<div className="col-lg-7 mb-4 mt-4 mx-auto">
 					<div className="card shadow mb-4">
-						<div className="card-header py-3">
-							<h6 className="m-0 font-weight-bold text-primary">{caseNo}</h6>
-							<h6 className="m-0 font-weight-bold text-primary">{AlienNo}</h6>
-							<h6 className="m-0 font-weight-bold text-primary">{LastName}</h6>
-							<h6 className="m-0 font-weight-bold text-primary">{FirstName}</h6>
-							<h6 className="m-0 font-weight-bold text-primary">{MaritalStatus}</h6>
-							<h6 className="m-0 font-weight-bold text-primary">{City}</h6>
-							<h6 className="m-0 font-weight-bold text-primary">{State}</h6>
-							<h6 className="m-0 font-weight-bold text-primary">{ZipCode}</h6>
-							<h6 className="m-0 font-weight-bold text-primary">{Email}</h6>
-							<h6 className="m-0 font-weight-bold text-primary">{HomePhone}</h6>
-							<h6 className="m-0 font-weight-bold text-primary">{WorkPhone}</h6>
+						<div className="card-header center py-3">
+							<h6 className="m-0 font-weight-bold text-primary">Biographical Information</h6>
+						</div>
+						<div className="card-body">
+							<table className="table">
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										Case No.
+									</th>
+									<td className="small">{caseNo}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										Alien No.
+									</th>
+									<td className="small">{AlienNo}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										Last Name
+									</th>
+									<td className="small">{LastName}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										First Name
+									</th>
+									<td className="small">{FirstName}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										Gender
+									</th>
+									<td className="small">{Gender}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										Marital Status
+									</th>
+									<td className="small">{MaritalStatus}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										DOB
+									</th>
+									<td className="small">{DOB}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										Home Phone
+									</th>
+									<td className="small">{HomePhone}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										Work Phone
+									</th>
+									<td className="small">{WorkPhone}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										Email
+									</th>
+									<td className="small">{Email}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										Address
+									</th>
+									<td className="small">{address}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										City
+									</th>
+									<td className="small">{City}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										State
+									</th>
+									<td className="small">{State}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										Zip Code
+									</th>
+									<td className="small">{ZipCode}</td>
+								</tr>
+							</table>
 						</div>
 					</div>
 				</div>
 
-				<div className="container-fluid">
+				<div className="col-lg-7 mb-4 mt-4 mx-auto">
+					<div className="card shadow mb-4">
+						<div className="card-header center py-3">
+							<h6 className="m-0 font-weight-bold text-primary">Immigration Information</h6>
+						</div>
+						<div className="card-body">
+							<table className="table">
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										Immigration Status
+									</th>
+									<td className="small">{immigrationStatus}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										Date of Entry to USA
+									</th>
+									<td className="small">{dateEntry}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										Port of Entry to USA
+									</th>
+									<td className="small">{portEntry}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										Transportation
+									</th>
+									<td className="small">{transportation}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										City of Birth
+									</th>
+									<td className="small">{birthCity}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										Country of Birth
+									</th>
+									<td className="small">{birthCountry}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										Nationality
+									</th>
+									<td className="small">{nationality}</td>
+								</tr>
+							</table>
+						</div>
+					</div>
+				</div>
+
+				<div className="col-lg-7 mb-5 mt-4 mx-auto">
+					<div className="card shadow mb-4">
+						<div className="card-header center py-3">
+							<h6 className="m-0 font-weight-bold text-primary">Legal Criminal Record</h6>
+						</div>
+						<div className="card-body">
+							<table className="table">
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										Legal Problem
+									</th>
+									<td className="small">{legalProblem}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										Case Goal
+									</th>
+									<td className="small">{caseGoal}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										Follow-Up
+									</th>
+									<td className="small">{followUp}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										Arrest Record
+									</th>
+									<td className="small">{arrestRecord}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										Criminal Attorneys Name
+									</th>
+									<td className="small">{criminalAttorney}</td>
+								</tr>
+								<tr>
+									<th scope="row" className="small font-weight-bold">
+										Criminal Attorney Phone
+									</th>
+									<td className="small">{attorneyPhone}</td>
+								</tr>
+							</table>
+						</div>
+					</div>
+				</div>
+
+				{/* <h6 className="m-0 font-weight-bold text-primary">{caseNo}</h6>
+					<h6 className="m-0 font-weight-bold text-primary">{AlienNo}</h6>
+					<h6 className="m-0 font-weight-bold text-primary">{LastName}</h6>
+					<h6 className="m-0 font-weight-bold text-primary">{FirstName}</h6>
+					<h6 className="m-0 font-weight-bold text-primary">{MaritalStatus}</h6>
+					<h6 className="m-0 font-weight-bold text-primary">{City}</h6>
+					<h6 className="m-0 font-weight-bold text-primary">{State}</h6>
+					<h6 className="m-0 font-weight-bold text-primary">{ZipCode}</h6>
+					<h6 className="m-0 font-weight-bold text-primary">{Email}</h6>
+					<h6 className="m-0 font-weight-bold text-primary">{HomePhone}</h6>
+					<h6 className="m-0 font-weight-bold text-primary">{WorkPhone}</h6> */}
+
+				{/* <div className="container-fluid">
 					<div className="card shadow mb-4">
 						<div className="card-header py-3">
 							<h6 className="m-0 font-weight-bold text-primary">{dateEntry}</h6>
@@ -117,9 +310,9 @@ export const CaseReport = () => {
 							<h6 className="m-0 font-weight-bold text-primary">{nationality}</h6>
 						</div>
 					</div>
-				</div>
+				</div> */}
 
-				<div className="container-fluid">
+				{/* <div className="container-fluid">
 					<div className="card shadow mb-4">
 						<div className="card-header py-3">
 							<h6 className="m-0 font-weight-bold text-primary">{legalProblem}</h6>
@@ -130,7 +323,7 @@ export const CaseReport = () => {
 							<h6 className="m-0 font-weight-bold text-primary">{attorneyPhone}</h6>
 						</div>
 					</div>
-				</div>
+				</div> */}
 			</div>
 		</div>
 	);
